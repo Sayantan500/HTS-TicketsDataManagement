8622061a0fa44f9810b11d0ea890509578db9655
sayantan <sayantandas657@outlook.com>
2023-04-22 22:51:35 +0530

    Get tickets by id

--- src/main/java/com/helpdesk_ticketing_system/tickets_data_management/persistence/Database.java
+++ src/main/java/com/helpdesk_ticketing_system/tickets_data_management/persistence/Database.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface Database<T> {
     T saveToDb(T newData) throws Exception;
     List<T> getFromDb(String deptId, String status, Integer limit, Long startRange, Long endRange, Class<T> targetType);
+    T getById(Object ticketId, Class<T> targetType);
 }
